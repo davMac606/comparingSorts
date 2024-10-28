a9a2c443cd5d2e2be542c4078976b95eaef5e307
davMac606 <cl201238@g.unicamp.br>
2024-10-28 18:06:21 -0300

    changed main method and implemented merge sort

--- methods.c
+++ methods.c
@@ -79,3 +79,67 @@ boolean Bubble(int *array, int n) {
   printf("Tempo de execucao: %f segundos.\n", tempo_cpu);
   return true;
 }
+
+void mergeSort(int* array, int left, int right) {
+  if (left < right) {
+    int pivot = (left + right) / 2;
+    mergeSort(array, left, pivot);
+    mergeSort(array, pivot+1, right);
+    breakdown(array, left, pivot, right);
+  }
+}
+
+void breakdown(int* array, int left, int pivot, int right) {
+  int lenL = pivot - left;
+  int lenR = right - pivot;
+  int leftPart[lenL], rightPart[lenR], i, j, k;
+
+  for (i = 0; i < lenL; i++) {
+    leftPart[i] = array[left+1];
+  }
+  for (j = 0; j < lenR; j++) {
+    rightPart[j] = array[pivot+j+1];
+  }
+  i,j = 0;
+  k = left;
+
+  while (i < lenL && j < lenR) {
+    if (leftPart[i] <= rightPart[j]) {
+      array[k] = leftPart[i];
+      i++;
+    } else {
+      array[k] = rightPart[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < lenL) {
+        array[k] = leftPart[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < lenR) {
+        array[k] = rightPart[j];
+        j++;
+        k++;
+    }
+}
+
+
+boolean Merge(int* array, int n) {
+  inicio = clock();
+  if (n <= 0) {
+    return false;
+  }
+  mergeSort(array, 0, n-1);
+  fim = clock();
+  tempo_cpu = ((double) (fim - inicio)) / CLOCKS_PER_SEC;
+  printf("\n\nMetodo: Merge.\nArray de tamanho %d ordenado com sucesso.\n", n);
+  printf("Tempo de execucao: %f segundos.\n", tempo_cpu);
+
+  return true;
+}
--- methods.h
+++ methods.h
@@ -5,8 +5,8 @@
 
 boolean Select(int* array, int n); //done
 boolean Insert(int* array, int n); //done
-boolean Bubble(int* array, int n); //done(i guess?)
-boolean Merge(int* array, int n);
+boolean Bubble(int* array, int n); //done
+boolean Merge(int* array, int n); //done
 boolean Quick(int* array, int n);
 boolean Heap(int* array, int n);
 
