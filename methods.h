e5b8f32bad96cc4ad382e13536cda979b68ba5e1
davMac606 <cl201238@g.unicamp.br>
2024-10-29 16:57:15 -0300

    i forgot i didn't mark merge as done

--- methods.h
+++ methods.h
@@ -6,7 +6,7 @@
 boolean Select(int* array, int n); //done
 boolean Insert(int* array, int n); //done
 boolean Bubble(int* array, int n); //done(i guess?)
-boolean Merge(int* array, int n);
+boolean Merge(int* array, int n); //done
 boolean Quick(int* array, int n);
 boolean Heap(int* array, int n);
 
